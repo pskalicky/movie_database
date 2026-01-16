d3162687be8751546de7c1fd6fdd20491800d00c
Petr Skalicky <petrskali@Petrs-MacBook-Air.local>
2026-01-17 00:49:13 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movie_database',
+  webDir: 'www'
+};
+
+export default config;
