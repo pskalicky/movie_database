63178e4199b06c0341db781ad24475d30ea850e5
Petr Skalicky <petrskali@Petrs-MacBook-Air.local>
2026-01-18 16:42:17 +0100

    -finalni test na androidu

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,24 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'movie_database',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000, // Jak dlouho se zobrazuje (ms)
+      launchAutoHide: true,     // Automaticky schovat?
+      backgroundColor: "#ffffff", // Barva pozadí (hex) - měla by ladit se splash.png
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,       // Zda ukazovat točící se kolečko
+      splashFullScreen: true,
+      splashImmersive: true,
+    }
+  }
 };
 
 export default config;
